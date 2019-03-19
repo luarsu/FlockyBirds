ca492c7b79548b39248bd5ea0c450640c39c9c11
Luis Armas <luarsu@gmail.com>
2019-03-19 11:24:16 +0000

    Added comments to the code

--- Assets/Scripts/Level.cs
+++ Assets/Scripts/Level.cs
@@ -7,14 +7,18 @@ using UnityEngine.UI;
 //Class to have general control of the level
 
 public class Level : MonoBehaviour {
+    //score variables
     public Text tScore;
     public int score = 0;
+    //reference of the prefab to create enemies, birds, eggs etc.
     public Transform flockPrefab;
     public Transform enemyPrefab;
     public Enemy2 enemy2;
     public PositiveStimuli pstimul;
+    // number of enemies and birds
     public int numFlock;
     public int numEnemies;
+    //array for the enemies, birds and stimulus
     public List<flockMember> members;
     public List<Enemy> enemies;
     public List<PositiveStimuli> pstimulis;
@@ -25,14 +29,17 @@ public class Level : MonoBehaviour {
     // Use this for initialization
     void Start () {
 
+        //Create empty list of negative stimulus and birds
         levelEnd = false;
         members = new List<flockMember>();
         enemies = new List<Enemy>();
 
-
+        //spawn 100 birds to begin the level
         Spawn(flockPrefab, 100);
+
         //Spawn(enemyPrefab, numEnemies);
 
+        //Fills the list of birds and negative stimulus at the beggining. The list will be updated as birds/negative stimulus are created or destroyed.
         members.AddRange(FindObjectsOfType<flockMember>());
         enemies.AddRange(FindObjectsOfType<Enemy>());
         pstimulis.AddRange(FindObjectsOfType<PositiveStimuli>());
@@ -40,12 +47,13 @@ public class Level : MonoBehaviour {
 
     void Update()
     {
+        //Updates the score every frame if the game hasnt finished
         if (!levelEnd) {
             updateScore();
             tScore.text = score.ToString();
         }
         
-
+        /*HANDLE INPUT*/
 
         //Create negative stimuli with left click
         if (Input.GetMouseButtonDown(0)) {
@@ -76,22 +84,24 @@ public class Level : MonoBehaviour {
 
     }
 
+    //Updates the player score
     public void updateScore() {
         score = score + (members.Count)/5;
     }
 
-    //Get the neighbours that are inside the redius defined
+    //Get the neighbours (birds) that are inside the redius defined
     public List<flockMember> GetNeighbours(flockMember member, float radius) {
         List<flockMember> neighboursFound = new List<flockMember>() ;
         foreach (var otherMember in members) {
             if (otherMember == member)
                 continue;
-
+            //check if they are within the defined distance
             if (Vector3.Distance(member.position, otherMember.position) <= radius) {
                 neighboursFound.Add(otherMember);
             }
 
         }
+        //returns list of birds within the radious defined
         return neighboursFound;
     }
 
--- Assets/Scripts/flockMember.cs
+++ Assets/Scripts/flockMember.cs
@@ -28,7 +28,7 @@ public class flockMember : MonoBehaviour {
     void Update () {
         //Get the acceleration
         acceleration = Combine();
-        //Clamp it to the max acceleration
+        //restricts it to the max acceleration
         acceleration = Vector3.ClampMagnitude(acceleration, conf.maxAcceleration);
         //Calculate velocity vector
         velocity = velocity + acceleration * Time.deltaTime;
@@ -43,14 +43,122 @@ public class flockMember : MonoBehaviour {
 	   
 	}
 
-    //Claculate the vector for the avoidance of the enemies
+    /* METHODS TO CALCULATE DIRECTION, VELOCITY AND ACCELERATION*/
+
+
+    //Combines combines wander, cohesion, alignment, separation, avoidance and gotopositive stimuli based on their priorities. Returns the final acceleration vector
+    virtual protected Vector3 Combine()
+    {
+        Vector3 finalVec = conf.cohesionPriority * Cohesion() + conf.wanderPriority * Wander()
+            + conf.alignmentPriority * Alignment() + conf.separationPriority * Separation()
+            + conf.avoidancePriority * Avoidance() + conf.stimuliPriority * GoToPositiveStimuli();
+        return finalVec;
+    }
+
+    //Methos to return the random wander acceleration vector for the flock member /* RANDOM MOVEMENT*/
+    protected Vector3 Wander()
+    {
+        float jitter = conf.wanderJitter * Time.deltaTime;
+        wanderTarget += new Vector3(RandomBinomial() * jitter, RandomBinomial() * jitter, 0);
+        wanderTarget = wanderTarget.normalized;
+        //project random vector in the radius
+        wanderTarget *= conf.wanderRadious;
+        Vector3 targetInLocalSpace = wanderTarget + new Vector3(0, conf.wanderDistance, 0);
+        Vector3 targetInWorldSpace = transform.TransformPoint(targetInLocalSpace);
+        targetInWorldSpace -= this.position;
+        return targetInWorldSpace.normalized;
+
+    }
+
+    //TRIES TO GET CLOSER TO OTHER BIRDS. Calculates the cohesion vector to maintain the flock members close
+    Vector3 Cohesion()
+    {
+        Vector3 cohesionVector = new Vector3();
+        int countMembers = 0;
+        var neighbours = level.GetNeighbours(this, conf.cohesionRadius);
+        //No neighbours close to the member
+        if (neighbours.Count == 0)
+        {
+            return cohesionVector;
+        }
+        //Basically gets the position of all the birds that are withn the range defined, computes the averge position of all of them and goes towards it.
+        foreach (var member in neighbours)
+        {
+            if (isInFOV(member.position))
+            {
+                cohesionVector += member.position;
+                countMembers++;
+            }
+
+        }
+
+        if (countMembers == 0)
+            return cohesionVector;
+
+        //We calculate the cohesion vector with the member position and other members in FOV position and we normalize it
+        cohesionVector /= countMembers;
+        cohesionVector = cohesionVector - this.position;
+        cohesionVector = Vector3.Normalize(cohesionVector);
+        return cohesionVector;
+    }
+
+    //TRY TO GO IN THE SAME DIRECTION AS OTHER BIRDS. Compute the alignment acceleration vector based on the direction of the other birds.
+    Vector3 Alignment()
+    {
+        Vector3 alignVector = new Vector3();
+        var members = level.GetNeighbours(this, conf.alignmentRadius);
+        if (members.Count == 0)
+        {
+            return alignVector;
+        }
+        // Computes the average of the direction of all the birds within the defined radious. 
+        foreach (var member in members)
+        {
+            if (isInFOV(member.position))
+            {
+                alignVector += member.velocity;
+            }
+        }
+        return alignVector.normalized;
+    }
+
+
+    // TRIES TO STAY SEPARATED/ NOT COLLIDE WITH OTHER BIRDS. Computes the acceleration vector of separation vector based on the neighbours inside the separation radius
+    Vector3 Separation()
+    {
+        Vector3 separateVector = new Vector3();
+        var members = level.GetNeighbours(this, conf.separationRadius);
+        if (members.Count == 0)
+        {
+            return separateVector;
+        }
+
+        foreach (var member in members)
+        {
+            if (isInFOV(member.position))
+            {
+                //Tries to go in the oposite direction of the position of the birds that are too close. Calculates the average of all those directions.
+                Vector3 movingTowards = this.position - member.position;
+                if (movingTowards.magnitude > 0)
+                {
+                    separateVector += movingTowards.normalized / movingTowards.magnitude;
+                }
+            }
+        }
+        return separateVector.normalized;
+    }
+
+    
+
+    //TRIES TO ESTAY AWAY FROM ENEMIES. Calculate the vector for the avoidance of the enemies by computing the averange of the avoidance vector from the enemies
     Vector3 Avoidance() {
         Vector3 avoidVector = new Vector3();
+        //get enemies within the range
         var enemyList = level.GetEnemies(this, conf.avoidanceRadius);
         if (enemyList.Count == 0) {
             return avoidVector;
         }
-        //Calculate the vector based on the RunAway vector 
+        //Calculate the average of the avoidance direction (go in the oposite direction of where the enemies are) and returns it
         foreach (var enemy in enemyList) {
             avoidVector += RunAway(enemy.position);
 
@@ -58,7 +166,7 @@ public class flockMember : MonoBehaviour {
         return avoidVector.normalized;
     }
 
-    //Returns vector direction towards the closer positive stimuli
+    //Returns vector acceleration direction towards the closer positive stimuli
     Vector3 GoToPositiveStimuli() {
         Vector3 stimuliVector = new Vector3();
         var stimuliList = level.GetStimuli(this, conf.stimuliRadius);
@@ -90,97 +198,7 @@ public class flockMember : MonoBehaviour {
         return neededVelocity - velocity;
     }
 
-    //Methos to return the wander movement vector for the flock member
-    protected Vector3 Wander()
-    {
-        float jitter = conf.wanderJitter * Time.deltaTime;
-        wanderTarget += new Vector3(RandomBinomial() * jitter, RandomBinomial() * jitter, 0);
-        wanderTarget = wanderTarget.normalized;
-        //project random vector in the radius
-        wanderTarget *= conf.wanderRadious;
-        Vector3 targetInLocalSpace = wanderTarget + new Vector3(0, conf.wanderDistance,0);
-        Vector3 targetInWorldSpace = transform.TransformPoint(targetInLocalSpace);
-        targetInWorldSpace -= this.position;
-        return targetInWorldSpace.normalized;
-        
-    }
-
-    // computes the vector of separation vector based on the neighbours inside the separation radius
-    Vector3 Separation()
-    {
-        Vector3 separateVector = new Vector3();
-        var members = level.GetNeighbours(this, conf.separationRadius);
-        if (members.Count == 0)
-        {
-            return separateVector;
-        }
-
-        foreach (var member in members)
-        {
-            if (isInFOV(member.position))
-            {
-                //checks if the members are moving towards and computes the separate vector
-                Vector3 movingTowards = this.position - member.position;
-                if (movingTowards.magnitude > 0)
-                {
-                    separateVector += movingTowards.normalized / movingTowards.magnitude;
-                }
-            }
-        }
-        return separateVector.normalized;
-    }
-
-    //Compute the alignment vector based on the  alignment of the neighbours  inside the FOV combination of vectors
-    Vector3 Alignment()
-    {
-        Vector3 alignVector = new Vector3();
-        var members = level.GetNeighbours(this, conf.alignmentRadius);
-        if (members.Count == 0)
-        {
-            return alignVector;
-        }
-
-        foreach (var member in members)
-        {
-            if (isInFOV(member.position))
-            {
-                alignVector += member.velocity;
-            }
-        }
-        return alignVector.normalized;
-    }
-
-    //Calculates the cohesion vector to maintain the flock members close
-    Vector3 Cohesion()
-    {
-        Vector3 cohesionVector = new Vector3();
-        int countMembers = 0;
-        var neighbours = level.GetNeighbours(this, conf.cohesionRadius);
-        //No neighbours close to the member
-        if (neighbours.Count == 0)
-        {
-            return cohesionVector;
-        }
-        //Here we count the number of neighbours in the FOV and combine their position in the cohesion vector
-        foreach (var member in neighbours)
-        {
-            if (isInFOV(member.position))
-            {
-                cohesionVector += member.position;
-                countMembers++;
-            }
-
-        }
-
-        if (countMembers == 0)
-            return cohesionVector;
-
-        //We calculate the cohesion vector with the member position and other members in FOV position and we normalize it
-        cohesionVector /= countMembers;
-        cohesionVector = cohesionVector - this.position;
-        cohesionVector = Vector3.Normalize(cohesionVector);
-        return cohesionVector;
-    }
+    
 
 
     //Returns true if the neighbours is in the field of vision of the member
@@ -189,13 +207,7 @@ public class flockMember : MonoBehaviour {
 
     }
 
-    //Combines combines wander, cohesion, alignment, separation, avoidance and gotopositive stimuli based on their priorities. Returns the final acceleration vector
-    virtual protected Vector3 Combine() {
-        Vector3 finalVec = conf.cohesionPriority * Cohesion() + conf.wanderPriority * Wander()
-            + conf.alignmentPriority * Alignment() + conf.separationPriority * Separation()
-            +conf.avoidancePriority * Avoidance() + conf.stimuliPriority * GoToPositiveStimuli();
-        return finalVec;
-    }
+
 
     
     //Checks collision to remove positive stimulis on touch or remove self from the level list when the object is deleted
